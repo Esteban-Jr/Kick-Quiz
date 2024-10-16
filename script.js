b94e8b70b0419ca0673536a32e6bdab5c43196b6
Esteban <Esteban-g-7@hotmail.com>
2024-10-16 00:27:27 -0500

    Add quiz questions and options

--- script.js
+++ script.js
@@ -10,3 +10,58 @@ const optionContainer = document.querySelector(".option-container");
 
 let currentQuestionIndex = 0;
 let correctAnswers = 0;
+
+// Questions
+const questions = [
+    {
+    
+      question:'Who is the G.O.A.T of football?',
+      options:['Lionel Messi','Cristiano Ronaldo','Neymar Jr','Eden Hazard'],
+      answer: 0
+    },
+    {
+      question:'What is the best football league in the world?',
+      options:['La Liga','Premier League','Ligue 1','Bundesliga'],
+      answer: 1
+    },
+    {
+    question:'Which country won the Euros 2024?',
+      options:['France','England','Spain','Portugal'],
+      answer: 2
+    },
+    {
+      question:'Who won the Copa America 2024?',
+      options:['Colombia','Uruguay','Canada','Argentina'],
+      answer: 3
+    },
+    {
+      question:'Who has the most Ballon d\'Ors?',
+      options:['Lionel Messi','Lev Yashin','Johan Cruyff','Zinedine Zidane'],
+      answer: 0
+    },
+    {
+      question:'What football club has the most Champions league trophies?',
+      options:['Barcelona','Real Madrid','Chelsea','Arsenal'],
+      answer: 1
+    },
+    {
+      question:'How many World Cups have there been?',
+      options:['54','17','22','31'],
+      answer: 2
+    },
+    {
+      question:'Who is the All time Premier League goal scorer?',
+      options:['Harry Kane','Sergio Aguero','Frank Lampard','Alan Shearer'],
+      answer: 3
+    },
+    {
+      question:'Who is the most decorated player of all time?',
+      options:['Lionel Messi','Dani Alves','Andres Iniesta','David Alaba'],
+      answer: 0
+    },
+    {
+      question:'What month do the top european leagues start?',
+      options:['May','August','July','September'],
+      answer: 1
+    }
+];
